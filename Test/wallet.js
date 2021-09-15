d56654f588c257eaf7885c6504b9535e6a6e90b5
Jules MARTIN <julesmrt49@gmail.com>
2021-09-15 14:31:13 +0200

    new commit

---
+++ Test/wallet.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var Wallet = require ('../modeles/wallet');
+
+
+describe('Multiplication', function(){
+    var wallet 
+it('should work', function(){
+assert.equal(2*3, 6);
+});
+});
